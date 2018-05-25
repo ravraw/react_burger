09c977b3ec625a749d68510bb85aabca4bca6057
ravindra rawat <ravinrawat.wd@gmail.com>
2018-05-25 16:25:30 -0600

    basic layout setup added

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import classes from "./Layout.css";
+
+const Layout = props => {
+  return (
+    <React.Fragment>
+      <div>Toolbar,Sidebar,Backdrop</div>
+      <main className={classes.Content}>{props.children}</main>
+    </React.Fragment>
+  );
+};
+
+export default Layout;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>Burger</div>
+        <div>build controls</div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default BurgerBuilder;
