fbf80ad2df044b68ca373807252a91a8fd2e56a2
ravindra rawat <ravinrawat.wd@gmail.com>
2018-05-26 15:25:22 -0600

    Added state to "BurgerBuilder" and adding "burgerIngredient" component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,16 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 import classes from "./Burger.css";
 
-const burger = () => {
+const burger = props => {
+  let ingredientsNames = Object.keys(props.ingredients).map(ingKey => {
+    return [...Array(props.ingredients.ingKey)].map((_, i) => {
+      return <BurgerIngredient key={ingKey + i} type={ingKey} />;
+    });
+  });
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {ingredientsNames}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,19 @@ import React, { Component } from "react";
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 2,
+      cheese: 1,
+      meat: 2
+    }
+  };
+
   render() {
     return (
       <React.Fragment>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>build controls</div>
       </React.Fragment>
     );
