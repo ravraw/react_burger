e6b017e6ca9a403e01c2d38cf89533eab74ae17d
ravindra rawat <ravinrawat.wd@gmail.com>
2018-05-26 15:55:19 -0600

    validation added for ingredients quantity

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,18 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from "./Burger.css";
 
 const burger = props => {
-  let ingredientsNames = Object.keys(props.ingredients).map(ingKey => {
-    return [...Array(props.ingredients.ingKey)].map((_, i) => {
-      return <BurgerIngredient key={ingKey + i} type={ingKey} />;
-    });
-  });
+  let ingredientsNames = Object.keys(props.ingredients)
+    .map(ingKey => {
+      return [...Array(props.ingredients[ingKey])].map((_, i) => {
+        return <BurgerIngredient key={ingKey + i} type={ingKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (ingredientsNames.length === 0) {
+    ingredientsNames = <p>Please start adding ingredients...</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
