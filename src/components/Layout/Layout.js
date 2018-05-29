483adf16a7932522f559caf81ab000f38e6bcdf9
ravindra rawat <ravinrawat.wd@gmail.com>
2018-05-29 16:56:15 -0600

    Toggle functionality added to MENU button on toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -11,10 +11,19 @@ class Layout extends Component {
   sideDrawerClosedHandler = () => {
     this.setState({ showSideDrawer: false });
   };
+
+  sideDrawerToggleHandler = () => {
+    this.setState(prevState => {
+      return {
+        showSideDrawer: !prevState.showSideDrawer
+      };
+    });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <Toolbar />
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
         <SideDrawer
           open={this.state.showSideDrawer}
           closed={this.sideDrawerClosedHandler}
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import classes from "./Toolbar.css";
 import Logo from "../../Logo/Logo";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 
 import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = props => {
   return (
     <header className={classes.Toolbar}>
-      <div>MENU</div>
+      <DrawerToggle clicked={props.drawerToggleClicked} />
       <div className={classes.Logo}>
         <Logo />
       </div>
